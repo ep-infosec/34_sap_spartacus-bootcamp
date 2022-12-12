918d0d660de710faa2723340de3d1c66f72c8ce9
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-12 12:54:57 +0200

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# Sample code repository for Spartacus
+
+[![REUSE status](https://api.reuse.software/badge/github.com/SAP/spartacus-bootcamp)](https://api.reuse.software/info/github.com/SAP/spartacus-bootcamp)
+
+This is a sample code repository for Spartacus. Each folder containing a `package.json` is a sample project for Spartacus.
+
+In order to try out one of the sample projects, simply navigate to the desired repository and follow the instructions in that folder's `README.md`.
+
+## Sample projects
+
+### Spartacus Bootcamp
+
+Repository: `bootcamp`
+
+Sample code repository for Spartacus implementation and extensibility. It showcases a set of code examples and concrete use cases on how to implement, extend and enhance Spartacus for CX customers, partners, and implementers.
+
+### Sparta 0 (Introduction to Spartacus)
+
+Repository: `sparta0`
+
+Repository contains with the examples shown in OpenSAP [Introduction to "Spartacus" Course](https://open.sap.com/courses/sparta0).
+
+## Useful links
+
+- [Spartacus source code repository](https://github.com/SAP/spartacus)
+- [Spartacus documentation site](https://sap.github.io/spartacus-docs/)
+
+## Licensing
+
+Copyright 2020-2021 SAP SE or an SAP affiliate company and spartacus-bootcamp contributors. Please see our [LICENSE](LICENSE) for copyright and license information. Detailed information including third-party components and their licensing/copyright information is available [via the REUSE tool](https://api.reuse.software/info/github.com/SAP/spartacus-bootcamp).
---
+++ bootcamp/src/app/app.module.ts
@@ -0,0 +1,72 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import { ConfigModule } from '@spartacus/core';
+import { B2cStorefrontModule } from '@spartacus/storefront';
+import { environment } from 'src/environments/environment';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'spartacus-app' }),
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: environment.occBaseUrl,
+        },
+      },
+      context: {
+        baseSite: ['electronics-spa'],
+      },
+    }),
+
+    // demonstrate i18n
+    ConfigModule.withConfig({
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+    }),
+
+    // CustomBannerModule,
+
+    // BootcampWebComponentModule,
+
+    // BootcampRoutingModule,
+
+    // // Module to configure the site context with multi-site features
+    // BootcampMultiSiteModule,
+    // // Demonstrate a custom context
+    // CatalogContextModule,
+
+    // BootcampSeoModule,
+
+    // BootcampStateModule,
+
+    // BootcampDataBindingModule,
+
+    // Demonstrate static CMS config
+    // BootcampStaticCmsModule,
+
+    // BootcampConfigModule,
+    // // Provide a custom them to the `ThemeConfig`.
+    // ConfigModule.withConfig({ theme: Theme.STRAWBERRIES } as ThemeConfig),
+
+    // // Demonstrate outlets. Don't forget to enable the `<app-outlets></app-outlets>` in the app.component.html
+    // BootcampOutletModule,
+
+    // BootcampIconModule,
+
+    // BootcampI18nModule,
+
+    // demonstrates layout configuration
+    // BootcampLayoutConfigModule,
+
+    // BootcampComponentModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
